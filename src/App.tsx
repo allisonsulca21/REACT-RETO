157601a180b9f8a3005a60d5044fe7d617c7d3dd
allisonsulca21 <asulca2112@gmail.com>
2024-03-07 10:34:21 -0500

    Task 1 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import NeoGrid from './Grid';
+// import "ag-grid-community/styles/ag-grid.css";
 
 const App = () => {
   return (
-      <NeoGrid />
+    <>
+    <header className="ag-theme-quartz" style={{ height: 70, width: 1920, backgroundColor: 'black', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <p style={{ fontWeight: 'bold', fontSize: '30px' }}>Near-Earth Object Overview</p>
+    </header>
+    <NeoGrid />
+    </>
   );
 }
 
