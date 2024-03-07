1ede15eb1e5acac910078db8859aeb1153f1f402
allisonsulca21 <asulca2112@gmail.com>
2024-03-07 13:20:44 -0500

    Task 4 completed

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -8,16 +8,23 @@ const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation", sortable: true, filter: true },
   { field: "discovery_date", headerName: "Discovery Date", sortable: true, filter: 'agDateColumnFilter', 
     valueFormatter: params => {
-    return params.value ? new Date(params.value).toISOString().split('T')[0] : '';
-  }
-  },
+      return params.value ? new Date(params.value).toISOString().split('T')[0] : '';
+  }},
   { field: "h_mag", headerName: "H (mag)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "moid_au", headerName: "MOID (au)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "q_au_1", headerName: "q (au)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "q_au_2", headerName: "Q (au)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "period_yr", headerName: "Period (yr)", sortable: true, filter: 'agNumberColumnFilter' },
   { field: "i_deg", headerName: "Inclination (deg)", sortable: true, filter: 'agNumberColumnFilter' },
-  { field: "pha", headerName: "Potentially Hazardous",sortable: true, filter: true },
+  { field: "pha", headerName: "Potentially Hazardous",sortable: true, filter: true,
+    valueFormatter: params => {
+      return (
+        params.value === 'Y' ? 'Yes':
+        params.value === 'N' ? 'No':
+        params.value === 'n/a' ? '':
+        params.value
+      );
+  }},
   { field: "orbit_class", headerName: "Orbit Class", sortable: true, filter: true },
 ];
 
